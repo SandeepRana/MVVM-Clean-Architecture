21ff0f6dde66987cb0fb47d696af39ff439e4a53
sandeep_kj <sandeeprana2028@gmail.com>
2025-06-11 12:09:28 +0530

    Correct clean architecture pattern

---
+++ app/src/main/java/com/example/myapplication/presentation/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.myapplication.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.example.myapplication.nav.NavGraph
+import com.example.myapplication.ui.theme.MyApplicationTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyApplicationTheme {
+                Scaffold(modifier = Modifier.Companion.fillMaxSize()) { innerPadding ->
+                    NavGraph(innerPadding = innerPadding)
+                }
+            }
+        }
+    }
+}
